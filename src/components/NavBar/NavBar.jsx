f0d14b2944e3ab440ffd8e15e7ad644091147a3b
amarraza312 <amarraza312@gmail.com>
2025-08-09 20:00:45 +0100

    commenting complete.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,34 @@
+//react and css imports
 import React from 'react'
 import './NavBar.css'
  
 
-
+// navbar component
 const NavBar = ({ onThemeToggle }) => {
   return (
-    <div className='wrap'>
+    <div className='wrap'> {/*navbar wrapper*/}
+    {/*logo*/}
         <a href="/"><img className='Logo' src="/LofiLogoText.gif" alt="logo" />
         </a>
         
-        <div className='NavMenu'>
+        <div className='NavMenu'>{/*wrapper for the menu section (buttons)*/}
+            {/*github link*/}
             <a target='_blank' rel='noreferrer' href='https://github.com/bimibro'>
                 <i className='fab fa-github'></i>
                 <span>GitHub</span>
             </a>
 
+            {/*portfolio link (currently inactive)*/}
             <a className='PortfolioA' target='' rel='noreferrer' href='/'>
                 <i className='fas fa-globe'></i>
                 <span>portfolio</span>
             </a>
-
+            {/*theme toggle button*/}
             <button id='ThemeToggle' className='ThemeToggle'  onClick={onThemeToggle}>
                 <img src='/ModeToggle.png' alt='dark mode icon' width={50}/>
                 <img src='/ModeToggle.png' alt='light mode icon' width={50}/>
             </button>
-
+            {/*fullscreen toggle button*/}
             <button className='FullScreenToggle' onClick={() => {
                 if (!document.fullscreenElement) {
                     document.documentElement.requestFullscreen();
@@ -34,6 +38,7 @@ const NavBar = ({ onThemeToggle }) => {
                     }
                 }
             }}>
+                {/*icon for fullscreen toggle*/}
                 <i className='fas fa-expand fa-lg'></i>
             </button>
         </div>
